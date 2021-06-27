fa9936f5a62ea2bc884e6d22046bb6a416cbead9
alifia01 <brilliant11@webmail.umm.ac.id>
2021-06-28 00:34:44 +0700

    update blog2

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -160,7 +160,6 @@
                             <div class="product__pagination blog__pagination">
                                 <a href="#">1</a>
                                 <a href="#">2</a>
-                                <a href="#">3</a>
                                 <a href="#"><i class="fa fa-long-arrow-right"></i></a>
                             </div>
                         </div>
