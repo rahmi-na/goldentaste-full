5181a7e1827f73ba0d402139853194f5c6a2f71a
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2021-06-28 01:57:52 +0800

    retweet

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -223,7 +223,7 @@
                             <div class="product__item">
                                 <div class="product__item__pic set-bg" data-setbg="img/product/product-12.jpg">
                                     <ul class="product__item__pic__hover">
-                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
                                     </ul>
                                 </div>
                                 <div class="product__item__text">
