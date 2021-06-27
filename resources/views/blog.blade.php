50daff093e7dd1b300e4b147878b8e94ec2e0ea0
alifia01 <brilliant11@webmail.umm.ac.id>
2021-06-28 00:40:26 +0700

    tes5

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -150,7 +150,7 @@
                                         <li><i class="fa fa-comment-o"></i> 5</li>
                                     </ul>
                                     <h5><a href="#">Lifestyle</a></h5>
-                                    <p>Gaya hidup seputar food kuliner dan tips gaya hidup sehat</p>
+                                    <p>Gaya hidup seputar food kuliner dan tips gaya hidup sehat.</p>
                                     <a href="#" class="blog__btn">READ MORE <span class="arrow_right"></span></a>
                                 </div>
                             </div>
