3c7ddda88a48e5fc3ea7139c3f0ea2e5130af578
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2021-06-28 02:28:31 +0800

    retweet2

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -185,7 +185,6 @@
                                 <div class="product__item__pic set-bg" data-setbg="img/product/product-9.jpg">
                                     <ul class="product__item__pic__hover">
                                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>
                                     </ul>
                                 </div>
                                 <div class="product__item__text">
