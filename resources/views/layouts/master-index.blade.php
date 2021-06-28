86311e8b2c5c82e03d413f2d201f514fb411a58e
alifia01 <brilliant11@webmail.umm.ac.id>
2021-06-28 23:37:55 +0700

    coba subscribem2

--- resources/views/layouts/master-index.blade.php
+++ resources/views/layouts/master-index.blade.php
@@ -89,6 +89,29 @@
 
     <!-- Content-index Section Begin -->
     @yield('content-index')
+    <center>
+    <!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://umm.us6.list-manage.com/subscribe/post?u=3fba746498fb223d537ddf358&amp;id=c69c2a743d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Get E-mail updates about our daily recipes</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3fba746498fb223d537ddf358_c69c2a743d" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+</center>
+
     <!-- Content-index Section End -->
 
     <!-- Footer Section Begin -->
