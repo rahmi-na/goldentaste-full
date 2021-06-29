6d17fca732f632016323bacccbb1bd975594f63a
alifia01 <brilliant11@webmail.umm.ac.id>
2021-06-29 22:00:18 +0700

    tes lg

--- resources/views/layouts/master-index.blade.php
+++ resources/views/layouts/master-index.blade.php
@@ -102,7 +102,6 @@
     <div id="mc_embed_signup_scroll">
 	<!-- <label for="mce-EMAIL">Get E-mail updates about our daily recipes</label> -->
     <h6>Get E-mail updates about our daily recipes</h6>
-    <h5>click to subscribe Golden Taste</h5>
 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3fba746498fb223d537ddf358_c69c2a743d" tabindex="-1" value=""></div>
