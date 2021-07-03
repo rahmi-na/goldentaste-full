2d6256e8fc7440f9c6914cf690d0d1eb23a241a4
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2021-07-03 20:45:29 +0800

    Initial Commit5

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,27 +3,35 @@
 namespace Database\Seeders;
 
 use App\Models\Admins;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
 {
     public function run()
     {
-        $admins = [
-            [
+        Admins::truncate();
+        Admins::create([
                 'id'             => 1,
                 'name'           => 'Admin',
                 'username'       => 'admin1',
                 'password'       => bcrypt('12345'),
-            ],
-            [
-                'id'             => 2,
-                'name'           => 'User',
-                'username'       => 'user1',
-                'password'       => bcrypt('123'),
-            ],
-        ];
+        ]);
+        // $admins = [
+        //     [
+        //         'id'             => 1,
+        //         'name'           => 'Admin',
+        //         'username'       => 'admin1',
+        //         'password'       => bcrypt('12345'),
+        //     ],
+        //     [
+        //         'id'             => 2,
+        //         'name'           => 'User',
+        //         'username'       => 'user1',
+        //         'password'       => bcrypt('123'),
+        //     ],
+        // ];
 
-        Admins::insert($admins);
+        // Admins::insert($admins);
     }
 }
