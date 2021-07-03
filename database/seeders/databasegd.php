352a7d9e0ee24d1d2a15a07c90d0bbf0bcef685b
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2021-07-03 18:46:14 +0800

    Update database connection6

---
+++ database/seeders/databasegd.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'username'       => 'admin1',
+                'password'       => bcrypt('12345'),
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'User',
+                'username'       => 'user1',
+                'password'       => bcrypt('123'),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
